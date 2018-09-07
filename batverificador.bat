df7764e6162e7796ba9eea9669a7e5bcaa6a5c6d
Kiara KA. Arosemena <karosemena@cfe.local>
2018-09-07 15:23:23 -0500

    quitado REM en los comando verificadores.Reacomodado los titulos de descripcion del programa.Cambiado el orden de las arquitectura que estaban al revez.

--- batverificador.bat
+++ batverificador.bat
@@ -1,24 +1,25 @@
 cls
 echo off
-color 30
-echo ####################################
-echo #BAT VERIFICADOR DE LICENCIA OFFICE#
-echo #               2016               #
-echo ####################################
+cls
+color F5
+echo 						####################################
+echo 						#BAT VERIFICADOR DE LICENCIA OFFICE#
+echo 						#               2016               #
+echo 						####################################
 echo.
 echo.
-echo ###################################################
-echo ESTE ARCHIVO .BAT VERIFICARA EL NUMERO DE LICENCIA
-echo OFFICE 2016 INSTALADO EN EL EQUIPO
-echo INDICAR VERSION DE MAQUINA
-echo ###################################################
+echo 					####################################################
+echo 					#ESTE ARCHIVO .BAT VERIFICARA EL NUMERO DE LICENCIA#
+echo 					#        OFFICE 2016 INSTALADO EN EL EQUIPO        #
+echo 					#          INDICAR VERSION DE MAQUINA              #
+echo 					####################################################
 pause
 cls
 :MENU
 cls
 echo INDICAR QUE TIPO DE ARQUITECTURA TIENE EL EQUIPO:
-echo 1-x64
-echo 2-x86
+echo 1-x86
+echo 2-x64
 echo 3-Salir
 echo ESCRIBIR 1 o 2:
 set /p opcion=
@@ -31,15 +32,13 @@ pause
 goto :MENU
 
 :Re1
-	echo PRIMERA OPC
-REM	cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" \dstatus
-REM	cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" \dstatus
+	cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" \dstatus
+	cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" \dstatus
 	pause
 	goto :MENU
 :Re2
-	echo SEGUNDA OPC
-REM	cscript "C:\Archivos de Programa (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus
-REM	cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus	
+	cscript "C:\Archivos de Programa (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus
+	cscript "C:\Program Files (x86)\Microsoft Office\Office16\OSPP.VBS" /dstatus	
 	pause
 	goto :MENU
 :Salir
